1729efccbe19c6acbc3cd28da068336b6fc5cd17
lokiore <khuranalokesh07@gmail.com>
2018-04-10 01:54:22 +0530

    Kharab

--- app/src/main/java/com/example/rishabh/DispProfileActivity.java
+++ app/src/main/java/com/example/rishabh/DispProfileActivity.java
@@ -5,7 +5,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
@@ -13,12 +15,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.method.PasswordTransformationMethod;
 import android.text.style.UpdateAppearance;
 import android.view.View;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,6 +33,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 
@@ -34,19 +47,29 @@ import java.util.HashMap;
  */
 
 public class DispProfileActivity extends AppCompatActivity {
+    public static final int PICK_IMAGE_REQUEST = 1;
+    private ProgressBar mProgressBar;
+    private Uri mImageUri;
+    private StorageReference mStorageRef;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mDatabaseRef;
+    private StorageTask mUploadTask;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_profile);
+        mStorageRef = FirebaseStorage.getInstance().getReference("uploads");
+        mDatabase = FirebaseDatabase.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
         ImageView imageView = findViewById(R.id.disp_profile_picture);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.photo);
-        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(),bitmap);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.photo);
+        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
         roundedBitmapDrawable.setCircular(true);
         imageView.setImageDrawable(roundedBitmapDrawable);
 
         FirebaseUser mAuth = FirebaseAuth.getInstance().getCurrentUser();
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
-        if(acct!=null){
+        if (acct != null) {
             String personName = acct.getDisplayName();
             String personGivenName = acct.getGivenName();
             String personFamilyName = acct.getFamilyName();
@@ -63,9 +86,8 @@ public class DispProfileActivity extends AppCompatActivity {
             passwordView.setVisibility(View.GONE);
             LinearLayout updateView = findViewById(R.id.update_profile);
             updateView.setVisibility(View.GONE);
-        }
-        else{
-            if(mAuth!=null) {
+        } else {
+            if (mAuth != null) {
                 String new_email = mAuth.getEmail();
                 new_email = new_email.replaceAll("\\.", "_dot_");
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -88,13 +110,15 @@ public class DispProfileActivity extends AppCompatActivity {
                         username.setText(personName);
                         email.setText(personEmail);
                         mobile.setText(personMobile);
-                        personPassword = personPassword.replaceAll(".","*");
+                        personPassword = personPassword.replaceAll(".", "*");
                         password.setText(personPassword);
                         LinearLayout updateView = findViewById(R.id.update_profile);
                         updateView.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
                                 Intent intent = new Intent(DispProfileActivity.this, UpdateProfileActivity.class);
+
+
                                 startActivity(intent);
 
                             }
@@ -110,9 +134,8 @@ public class DispProfileActivity extends AppCompatActivity {
         }
 
 
-
-
     }
+    //ImageView mImageView = findViewById(R.id.disp_profile_picture);
 
 
 
--- app/src/main/java/com/example/rishabh/UpdateProfileActivity.java
+++ app/src/main/java/com/example/rishabh/UpdateProfileActivity.java
@@ -1,9 +1,12 @@
 package com.example.rishabh;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
@@ -12,12 +15,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.style.UpdateAppearance;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -26,6 +33,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
@@ -36,13 +49,23 @@ public class UpdateProfileActivity extends AppCompatActivity {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference mDatabase = database.getReference();
-
+    ImageView mImageView = findViewById(R.id.disp_profile_picture);
+    public static final int PICK_IMAGE_REQUEST = 1;
+    private ProgressBar mProgressBar;
+    private Uri mImageUri;
+    private StorageReference mStorageRef;
+    //private FirebaseDatabase mDatabase;
+    private DatabaseReference mDatabaseRef;
+    private StorageTask mUploadTask;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
-        final FirebaseUser mAuth = FirebaseAuth.getInstance().getCurrentUser();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
+        final FirebaseUser mAuth = FirebaseAuth.getInstance().getCurrentUser();
+        mStorageRef = FirebaseStorage.getInstance().getReference("uploads");
+
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
         ImageView imageView = findViewById(R.id.disp_profile_picture);
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.photo);
         RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
@@ -130,7 +153,86 @@ public class UpdateProfileActivity extends AppCompatActivity {
             }
         });
 
+        ImageView profilePicture = findViewById(R.id.disp_profile_picture);
+        profilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
 
+
+
+
+    }
+
+    private void openFileChooser(){
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,PICK_IMAGE_REQUEST);
+        uploadFile();
+
+    }
+
+
+    private void uploadFile(){
+        if(mImageUri != null){
+            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()+"."+getFileExtention(mImageUri));
+
+            mUploadTask = fileReference.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            mProgressBar.setProgress(0);
+                        }
+                    },500);
+                    Toast.makeText(UpdateProfileActivity.this,"Upload successful",Toast.LENGTH_LONG).show();
+                    Upload upload = new Upload("Pic",taskSnapshot.getDownloadUrl().toString());
+                    String uploadId = mDatabaseRef.push().getKey();
+                    mDatabaseRef.child(uploadId).setValue(upload);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(UpdateProfileActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0*taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                    mProgressBar.setProgress((int) progress);
+
+                }
+            });
+
+
+        }else{
+            Toast.makeText(this,"No file selected",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+            mImageUri = data.getData();
+
+            Picasso.get().load(mImageUri).into(mImageView);
+        }
+
+    }
+
+
+    private  String getFileExtention(Uri uri){
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
     }
 }
 
