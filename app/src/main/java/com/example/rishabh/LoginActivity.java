4c48bb06a7f7d3dadb02f420357c18b70b3e7a34
lokiore <khuranalokesh07@gmail.com>
2018-04-09 21:18:34 +0530

    News Feed layout

--- app/src/main/java/com/example/rishabh/LoginActivity.java
+++ app/src/main/java/com/example/rishabh/LoginActivity.java
@@ -281,7 +281,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        // Disable going back to the MainActivity
+        // Disable going back to the MainActivity1
         moveTaskToBack(true);
     }
 
